da2e35a5cf3e82a2a1a0e3280deab3ae58f058ed
tushushu <m18591915960@163.com>
2019-04-23 15:25:34 +0800

    完成

--- no_gil_lock.py
+++ no_gil_lock.py
@@ -1,8 +1,8 @@
-
 import pyximport
 pyximport.install()
 
 from _no_gil_lock import _add_one  # pylint: disable=import-error
 
-def add_one(n_times= 10 ** 7):
+
+def add_one(n_times=10 ** 7):
     return _add_one(n_times)
