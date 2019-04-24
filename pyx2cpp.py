ad3447d188e84ec35e7618d837d1a8bdeb36c0b8
tushushu <m18591915960@163.com>
2019-04-24 16:08:25 +0800

    兼容cpp代码

---
+++ pyx2cpp.py
@@ -0,0 +1,14 @@
+# Makes pyximport compatible with cpp! 
+# import pyx2cpp; pyx2cpp.install()
+
+import pyximport
+from pyximport import install
+
+old_get_distutils_extension = pyximport.pyximport.get_distutils_extension
+
+def new_get_distutils_extension(modname, pyxfilename, language_level=3):
+    extension_mod, setup_args = old_get_distutils_extension(modname, pyxfilename, language_level)
+    extension_mod.language='c++'
+    return extension_mod,setup_args
+
+pyximport.pyximport.get_distutils_extension = new_get_distutils_extension
