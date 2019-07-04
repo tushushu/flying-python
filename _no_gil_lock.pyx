677911f9f0a61a0c8c1b99a85ceb26708d88d24c
tushushu <m18591915960@163.com>
2019-07-04 11:46:26 +0800

    delete useless file and tidy the code

--- _no_gil_lock.pyx
+++
@@ -1,8 +0,0 @@
-
-def _add_one(long n_times= 10 ** 7):
-    cdef long ret = 0
-    with nogil:
-        while ret < n_times:
-            ret += 1
-    return ret
-
--- hello.pyx
+++
@@ -1,8 +0,0 @@
-"""
-@Author: tushushu
-@Date: 2019-04-24 16:11:22
-"""
-
-
-def say_hello_to(name):
-    return "Hello %s!" % name
